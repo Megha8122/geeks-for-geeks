6f633d0308978cda1aad9eb227f4a02621b34161
Megha Saxena <103634353+Megha8122@users.noreply.github.com>
2024-03-29 22:24:41 +0530

    Create Implement strstr.cpp
    link- https://www.geeksforgeeks.org/problems/implement-strstr/1?page=1&difficulty=Basic&sortBy=submissions

---
+++ Implement strstr.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+   
+     for(int i = 0; i < s.size(); i++){
+         bool flag = false;
+             for(int j = 0; j < x.size() ; ){
+                 if(s[i+j] != x[j]){
+                      flag = false;
+                     break;
+                 }else{
+                     j++;
+                     flag = true;
+                 }
+             }
+         if(flag == true){
+             return i;
+         }
+     }
+     return -1;
+}
