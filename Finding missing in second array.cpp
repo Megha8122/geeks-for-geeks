f9485f29cf1b43cce3041507d95b183f531ab7ad
Megha Saxena <103634353+Megha8122@users.noreply.github.com>
2024-04-19 23:11:41 +0530

    Create Finding missing in second array.cpp
    link- https://www.geeksforgeeks.org/problems/in-first-but-second5423/1

---
+++ Finding missing in second array.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	vector<int> findMissing(int a[], int b[], int n, int m) 
+	{ 
+	    unordered_map<int,int> mm;
+	    vector<int> ans;
+	    for(int i = 0;i < n ;i++) mm[a[i]] = 1;
+	    for(int i = 0;i < m ;i++) mm[b[i]] = 0;
+	    for(int i = 0;i < n ;i++){
+	        if(mm[a[i]] == 1)ans.push_back(a[i]);
+	    }
+	    return ans;
+	} 
+};
+	
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        int a[n];
+        int b[m];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        for(int i=0;i<m;i++)
+        cin >> b[i];
+        vector<int> ans;
+        
+
+        Solution ob;
+        ans=ob.findMissing(a,b,n,m);
+        for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
