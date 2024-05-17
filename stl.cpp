6d7920ed1e69732842c172bc3180159186f107af
Megha Saxena <103634353+Megha8122@users.noreply.github.com>
2024-05-17 23:27:07 +0530

    Add files via upload

---
+++ stl.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void explainPair(){
+
+    pair<int, int> p = {1,3};
+    cout << p << endl;
+}
+
+
+int main(){
+    explainPair();
+    return 0;
+}
